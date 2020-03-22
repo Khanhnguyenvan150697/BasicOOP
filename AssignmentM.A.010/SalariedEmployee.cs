cf212c51da47a95aed81caa5cbd5b11e54890680
Khanhnguyenvan150697 <Duongminh150697@gmail.com>
2020-03-22 20:23:00 +0700

    baitap

---
+++ AssignmentM.A.010/SalariedEmployee.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentM.A._010
+{
+    public class SalariedEmployee : Employee
+    {
+        public static SalariedEmployee[] arrSalaried;
+        public double commissionRate { get; set; }
+        public double GrossSales { get; set; }
+        public double BasicSalary { get; set; }
+        public string department { get; set; }
+
+        public SalariedEmployee InputData()
+        {
+            SalariedEmployee newEmpl = new SalariedEmployee();
+            Console.WriteLine("Department name: ");
+            newEmpl.department = Console.ReadLine();
+            Console.Write("SSN: ");
+            newEmpl.SSN = Console.ReadLine();
+            Console.Write("First name: ");
+            newEmpl.FirstName = Console.ReadLine();
+            Console.Write("Last name: ");
+            newEmpl.LastName = Console.ReadLine();
+            Console.Write("Phone: ");
+            newEmpl.Phone = Console.ReadLine();
+            Console.Write("Email: ");
+            newEmpl.Email = Console.ReadLine();
+            Console.Write("Birth date: ");
+            newEmpl.BirthDate = Console.ReadLine();
+            Console.Write("Commission rate: ");
+            newEmpl.commissionRate = double.Parse(Console.ReadLine());
+            Console.Write("Gross sales: ");
+            newEmpl.GrossSales = double.Parse(Console.ReadLine());
+            Console.Write("Basic salary: ");
+            newEmpl.BasicSalary = double.Parse(Console.ReadLine());
+            return newEmpl;
+        }
+        public void Input()
+        {
+            Console.WriteLine("Department name: ");
+            department = Console.ReadLine();
+            Console.Write("SSN: ");
+            SSN = Console.ReadLine();
+            Console.Write("First name: ");
+            FirstName = Console.ReadLine();
+            Console.Write("Last name: ");
+            LastName = Console.ReadLine();
+            Console.Write("Phone: ");
+            Phone = Console.ReadLine();
+            Console.Write("Email: ");
+            Email = Console.ReadLine();
+            Console.Write("Birth date: ");
+            BirthDate = Console.ReadLine();
+            Console.Write("Commission rate: ");
+            commissionRate = double.Parse(Console.ReadLine());
+            Console.Write("Gross sales: ");
+            GrossSales = double.Parse(Console.ReadLine());
+            Console.Write("Basic salary: ");
+            BasicSalary = double.Parse(Console.ReadLine());
+        }
+        public void AddToArray()
+        {
+            Console.Write("Enter numbers of salaried employee: ");
+            int numbersEmployee = Int32.Parse(Console.ReadLine());
+            arrSalaried = new SalariedEmployee[numbersEmployee];
+            for (int i = 0; i < arrSalaried.Length; i++)
+            {
+                Console.WriteLine("Employee {0}", i + 1);
+                arrSalaried[i] = new SalariedEmployee();
+                arrSalaried[i].Input();
+            }
+        }
+        public void Output()
+        {
+            foreach(var item in arrSalaried)
+            {
+                Console.WriteLine($"Department name: {item.department}, " +
+                    $"SSN: {item.SSN}, {item.FirstName} {item.LastName}," +
+                    $" Phone: {item.Phone}, Email: {item.Email}, " +
+                    $"BirthData: {item.BirthDate}, " +
+                    $"Commission:{item.commissionRate}, " +
+                    $"Gross: {item.GrossSales}, " +
+                    $"salary: {item.BasicSalary}");
+            }
+        }
+    }
+}
---
+++ StringDateTime/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringDateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime dt = new DateTime(2020, 10, 6);
+            Console.WriteLine(dt.ToString("D", CultureInfo.CreateSpecificCulture("vi-VN")));
+
+            string dt1 = "15062020 05:30:20";
+            DateTime dt2;
+            DateTime.TryParseExact(dt1, "ddMMyyyy hh:mm:ss", CultureInfo.InvariantCulture, DateTimeStyles.None, out dt2);
+            Console.WriteLine(dt2);
+
+
+            TimeSpan ts1 = new TimeSpan(1, 10, 20, 3);
+            Console.WriteLine("Ngay: " + ts1.Days + " gio: " + ts1.Hours + " phut: " + ts1.Minutes + " giay: " + ts1.Seconds);
+
+            TimeSpan ts2 = new TimeSpan(48, 12, 30);
+            if(ts1.CompareTo(ts2) == -1)
+            {
+                Console.WriteLine("ts1 < ts2");
+            }
+            if (ts1.CompareTo(ts2) == 0)
+            {
+                Console.WriteLine("ts1 = ts2");
+            }
+            if (ts1.CompareTo(ts2) == 1)
+            {
+                Console.WriteLine("ts1 > ts2");
+            }
+
+            // Subtract() method trả về 1 TimeSpan, ts2 - ts1
+            Console.WriteLine(ts2.Subtract(ts1));
+            Console.ReadKey();
+        }
+    }
+}
